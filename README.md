44b5bbd6c41ff8180d6e49b92722dd4035aa2742
Les Matheson <les.matheson@gmail.com>
2022-08-16 21:40:15 -0400

    Initial scaffolding from shellkit:0.8.0

---
+++ README.md
@@ -0,0 +1,13 @@
+# hello-shellkit
+
+## Setup
+
+Download and install the self-extracting setup script:
+
+    https://github.com/sanekits/hello-shellkit/releases/latest/downloads/hello-shellkit-setup-0.1.0.sh
+
+Or **if** [shellkit-pm](https://github.com/sanekits/shellkit-pm) is installed:
+
+    shpm install hello-shellkit
+
+##
---
+++ bin/hello-shellkit-version.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Running hello-shellkit-version.sh is the correct way to
+# get the home install path for the tool
+KitVersion=0.1.0
+
+canonpath() {
+    # Like "readlink -f", but portable
+    ( cd -L -- "$(command dirname -- ${1})"; echo "$(command pwd -P)/$(command basename -- ${1})" )
+}
+
+Script=$(canonpath "$0")
+Scriptdir=$(dirname -- "$Script")
+
+
+if [ -z "$sourceMe" ]; then
+    printf "%s\t%s\n" ${Scriptdir}/hello-shellkit $KitVersion
+fi
